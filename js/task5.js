080e5e3bfbb6e09993fb3d1a4490427df45433c4
“Yulya <yeellowfish@gmail.com>
2018-11-23 18:07:02 +0800

    #add add task5

---
+++ js/task5.js
@@ -0,0 +1,20 @@
+//Возведение чисел в строке в степень
+// @param incomingString (string) - строка, числа в которой нужно возвести в степень
+// @param pow (number) - показатель степени
+
+function replaceDigitsInStringToPow (incomingString, pow) {
+	var regExp = /\d+/g,
+		arr = incomingString.match(regExp),
+		resultString = '',
+		i = 0;
+	
+	return incomingString.replace(regExp, function () {
+		return String(Math.pow(Number(arr[i++]), pow));
+	});
+}
+
+document.getElementById('check').onclick = function() {
+	var newElem = document.createElement('p');
+	newElem.innerHTML = replaceDigitsInStringToPow(document.getElementById('input').value, 3);
+	document.getElementById('result').appendChild(newElem);
+};	
